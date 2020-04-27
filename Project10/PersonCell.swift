e5fa4c925c502bd61ce844d190a4cf0c945e0ad7
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-04-27 20:18:27 +0900

    created PersonCell class and connected it to the storyboard

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Yoo Hwa Park on 2020/04/27.
+//  Copyright © 2020 Yoo Hwa Park. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
